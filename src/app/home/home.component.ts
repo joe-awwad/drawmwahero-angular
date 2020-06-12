30746db7cd57539e7222026fd6fe5f7fa435825f
Joe Awwad <joe.awwad@getray.com>
2020-06-12 16:37:04 +0300

    Refresh slideshow on HomeComponent Init. Closes #7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('.cycle-slideshow').cycle();
   }
-
 }
